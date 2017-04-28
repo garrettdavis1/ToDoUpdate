4b54d027fcae1f35d7246b6d803f555c569d66e6
CongoCode <benis.munganga@washburn.edu>
2017-04-28 16:26:39 -0500

    Update MasterToDo.java

--- MasterToDo.java
+++ MasterToDo.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package FinalProject;
+package ToDoUpdate;
 
 import java.util.ArrayList;
 import javafx.application.Application;
